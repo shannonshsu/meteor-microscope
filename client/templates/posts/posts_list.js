3adaa84ddad2d65e920c879e3973f5ce5d6b21ce
shannonshsu <shannonshsu@gmail.com>
2015-10-14 00:42:03 -0700

    added posts

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,18 +3,21 @@
 var postsData = [
 {
 	title: 'Intro Telescope',
-	url: 'http://neopets.com'
+	url: 'http://neopets.com/23423424'
 },
 {
 	title: 'Intro Telescope2',
-	url: 'http://neopets2.com'
+	url: 'http://neopets2.com/34.html'
 },
 {
 	title: 'Intro Telescope3',
-	url: 'http://neopets3.com'
-},
+	url: 'http://neopets3.com/2344.php'
+}
 ];
 
 Template.postsList.helpers({
 	posts: postsData
 });
+
+
+// posts is helper object that is available for template to use; used in postsList template
